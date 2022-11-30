ec37aef3d43429522f051c89dbf27c580867d455
Ishiyuka <shijing05youjia@gmail.com>
2022-11-30 19:13:04 +0900

    CSS、JavaScript、Helperのファイルを削除 & rails g コマンドで自動生成しないように設定

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,10 @@ module Kaizen
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "tasks#index"
+  root to: "issues#index"
   resources :issues
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
