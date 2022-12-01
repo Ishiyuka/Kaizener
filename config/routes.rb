2a8970ec895f0402e58e7c6c6b59989947d2df2c
Ishiyuka <shijing05youjia@gmail.com>
2022-12-01 11:38:37 +0900

    deviseコントローラー作成とルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "issues#index"
   resources :issues
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  root 'issues#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  resources :users, only: :show
+
+  devise_scope :user do
+    # root "users/sessions#new"
+    get "signup", to: "users/registrations#new"
+    get "login", to: "users/sessions#new"
+    delete "logout", to: "users/sessions#destroy"
   end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
