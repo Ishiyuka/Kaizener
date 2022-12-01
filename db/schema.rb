40eb0550ded549a32e87c01856dbb1068fda7cb9
Ishiyuka <shijing05youjia@gmail.com>
2022-12-01 21:49:10 +0900

    一般ユーザーのアクセス制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_101832) do
+ActiveRecord::Schema.define(version: 2022_12_01_074553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2022_11_30_101832) do
     t.integer "priority", null: false
     t.integer "status", null: false
     t.boolean "done_flag", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
