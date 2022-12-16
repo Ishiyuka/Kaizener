59deebba0937f67702f9683650634b3589d7adaf
Ishiyuka <shijing05youjia@gmail.com>
2022-12-16 21:49:20 +0900

    Fix: env/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'アプリケーションのドメイン名'}
+  config.action_mailer.default_url_options = { host: 'http://54.150.235.247' }
 
   config.action_mailer.perform_deliveries = true
 
