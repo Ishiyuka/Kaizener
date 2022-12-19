f57491b2b9e063173ec66c3550a3f56583f5bdb9
Ishiyuka <shijing05youjia@gmail.com>
2022-12-20 01:43:33 +0900

    Add: System spec for Issue & Plan

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
   factory :plan do
-    action { "MyText" }
-    pic { "MyString" }
+    action { "action" }
+    pic { "1" }
     due_date_at { "2022-12-02" }
     status { 1 }
     feedback { 1 }
+    user
+    team
+    issue
   end
 end
